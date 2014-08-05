9a6762b8c96be5c283e42d6e072d405a097e2e21
nitrous.io by filimo <filimo@gmail.com>
2014-08-05 11:41:53 +0000

    Fixed some bugs.

--- collections/WowChat.js
+++ collections/WowChat.js
@@ -2,6 +2,7 @@ WowChat = new Meteor.Collection('wowChat')
 
 Meteor.methods({
     saveMessage: function(message) {
+        console.log('saveMessage', message)
         var post = _.extend(_.pick(message, 'text'))
 
         WowChat.update(message._id, post)
